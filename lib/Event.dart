6b8257d55a93bff7faf30ee14fb3979f6cf19427
Eilein <eilein_fraser@hotmail.co.uk>
2023-11-20 13:40:17 +0000

    added Event file and removed redundant Drawer.dart

---
+++ lib/Event.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Event extends StatefulWidget {
+  const Event({super.key});
+
+  @override
+  State<Event> createState() => _EventState();
+}
+
+class _EventState extends State<Event> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
