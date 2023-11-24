4db10ed2b8f2a9d5a07ef2c58ee388cc0da3a6e1
Eilein <eilein_fraser@hotmail.co.uk>
2023-11-24 17:53:55 +0000

    problems with emulator fixed, db now loading in properly

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,10 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 void main() async {
-
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  // Future<void>
-//   await dotenv.load();
-  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
