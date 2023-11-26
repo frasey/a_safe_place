b61a91bbf470429f487975cff71ba523810c4a19
Eilein <eilein_fraser@hotmail.co.uk>
2023-11-26 13:02:14 +0000

    started dataService

---
+++ lib/Database/FBDataService.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class FBDataService extends StatelessWidget {
+  const FBDataService({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    void addEdit();
+  }
+  }
