3334e40a467a98f9d13d9538fa48819db6d74ef1
unknown <stephaniejbowring@gmail.com>
2023-11-28 10:05:54 +0000

    started setting up google authentication

--- lib/Authentication/login_page.dart
+++ lib/Authentication/login_page.dart
@@ -5,6 +5,7 @@ import 'package:a_safe_place/Global/Common/toast.dart';
 import 'package:a_safe_place/Homepage/HomePage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -83,6 +84,38 @@ class _LoginPageState extends State<LoginPage> {
                                     fontWeight: FontWeight.bold),
                               ))),
               ),
+              SizedBox(
+                height: 10,
+              ),
+              GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                      width: double.infinity,
+                      height: 45,
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Center(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              FontAwesomeIcons.google,
+                              color: Colors.white,
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Text(
+                              "Sign in with Google",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ))),
               SizedBox(
                 height: 20,
               ),
